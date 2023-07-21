619224035c2ba296e74928d31d589a9fe13d5626
Joris Mak <47725838+jorismak@users.noreply.github.com>
2023-07-21 16:49:14 +0200

    tweak style to my liking

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,6 +8,14 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'yoda_style' => [
+            'equal' => false,
+            'identical' => false,
+            'less_and_greater' => false,
+        ],
+        'increment_style' => [
+            'style' => 'post',
+        ],
     ])
     ->setFinder($finder)
 ;
